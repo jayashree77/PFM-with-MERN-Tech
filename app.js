f760f3a3575bd79a0a7e4b4bb410dd351fdc71a8
jayashree77 <sheelvantjayashree04@gmail.com>
2025-05-08 17:01:57 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,60 @@
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./DB/Database.js";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import transactionRoutes from "./Routers/Transactions.js";
+import userRoutes from "./Routers/userRouter.js";
+import path from "path";
+
+dotenv.config({ path: "./config/config.env" });
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+// Connect to database
+connectDB();
+
+// Allowed origins
+const allowedOrigins = [
+  "http://localhost:3000", // Allow local frontend
+  "https://main.d1sj7cd70hlter.amplifyapp.com",
+  "https://expense-tracker-app-three-beryl.vercel.app",
+];
+
+// Middleware
+app.use(express.json());
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Routes
+app.use("/api/v1", transactionRoutes);
+app.use("/api/auth", userRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
